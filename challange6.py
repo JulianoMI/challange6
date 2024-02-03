3c735f5a6551f4fc939d8ca5952609a3c67401b8
JulianoMI <139358522+JulianoMI@users.noreply.github.com>
2024-02-03 10:40:22 +0700

    Add files via upload

---
+++ challange6.py
@@ -0,0 +1,6 @@
+num = int(input("Masukkan Angka Ganjil Lebih Dari 20: "))
+
+while num % 2 == 0 or num < 20:
+    num = int ( input("Masukkan angka ganjil lebih dari 20 : "))
+else: 
+    print ("True")
